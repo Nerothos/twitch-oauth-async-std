428781e536194422906f315731908cffb33f6ada
Nerothos <nerothos@live.fr>
2020-06-14 20:24:44 +0200

    Big Bang

---
+++ README.md
@@ -0,0 +1,15 @@
+[![Build Status](https://travis-ci.org/nerothos/twitch-oauth-async-std.svg?branch=master)](https://travis-ci.org/nerothos/twitch-oauth-async-std)
+
+Twitch OAuth token
+===
+
+Asynd-std compatible Twitch Ouath request, validate and revoke lib using Surf
+
+Installation
+---
+
+```
+twitch-oauth-async-std = "0.1"
+```
+
+[Documentation](https://docs.rs/twitch-oauth-async-std/)
---
+++ src/lib.rs
@@ -0,0 +1,131 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fmt;
+use url::Url;
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct ValidatedToken {
+    pub client_id: String,
+    pub login: Option<String>,
+    pub user_id: Option<String>,
+    pub scopes: Vec<String>,
+}
+
+// To use the `{}` marker, the trait `fmt::Display` must be implemented
+// manually for the type.
+impl fmt::Display for ValidatedToken {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let string1 = format!("access_token: {}\n", self.client_id);
+        let string2 = format!("{} token_type: {:?}\n", string1, self.login);
+        let string3 = format!("{} expires_in: {:?}\n", string2, self.user_id);
+        let string4 = format!("{} expires_in: {:?}\n", string3, self.scopes);
+
+        write!(f, "{}", string4)
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct AppAccessToken {
+    pub access_token: String,
+    pub expires_in: usize,
+    pub scope: Option<Vec<String>>,
+    pub token_type: String,
+}
+// To use the `{}` marker, the trait `fmt::Display` must be implemented
+// manually for the type.
+impl fmt::Display for AppAccessToken {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let string1 = format!("access_token: {}\n", self.access_token);
+        let string2 = format!("{} expires_in: {}\n", string1, self.expires_in);
+        let string3 = format!("{} expires_in: {:?}\n", string2, self.scope);
+        let string4 = format!("{} token_type: {}\n", string3, self.token_type);
+        write!(f, "{}", string4)
+    }
+}
+
+/// To retrieve a token, you need to provide your client_id and client_secret as well as a scope array
+///
+/// ```rust
+/// let token = twitch_oauth_async_std::get_app_access_token("client_id", "client_secret");
+/// ```
+pub async fn get_app_access_token(
+    client_id: &str,
+    client_secret: &str,
+) -> Result<AppAccessToken, Box<dyn std::error::Error>> {
+    let mut params = HashMap::new();
+    params.insert("grant_type", "client_credentials");
+    params.insert("client_id", client_id);
+    params.insert("client_secret", client_secret);
+    let url = Url::parse_with_params("https://id.twitch.tv/oauth2/token", &params).unwrap();
+
+    let client = surf::Client::new();
+    let resp: AppAccessToken = client.post(&url).recv_json().await?;
+
+    Ok(resp)
+}
+/// To retrieve a token, you need to provide your client_id and client_secret as well as a scope array
+///
+/// ```rust
+/// let token = twitch_oauth_async_std::get_app_access_token_with_scopes("client_id", "client_secret", vec!["scopes".to_string()]);
+/// ```
+pub async fn get_app_access_token_with_scopes(
+    client_id: &str,
+    client_secret: &str,
+    scopes: Vec<String>,
+) -> Result<AppAccessToken, Box<dyn std::error::Error>> {
+    let joinee_scopes = scopes.join(" ");
+
+    let mut params = HashMap::new();
+    params.insert("grant_type", "client_credentials");
+    params.insert("client_id", client_id);
+    params.insert("client_secret", client_secret);
+    params.insert("scope", joinee_scopes.as_str());
+    let url = Url::parse_with_params("https://id.twitch.tv/oauth2/token", &params).unwrap();
+
+    let client = surf::Client::new();
+    let resp: AppAccessToken = client.post(&url).recv_json().await?;
+
+    Ok(resp)
+}
+
+/// To validate a token, you need to provide your access token
+///
+/// ```rust
+/// let token = twitch_oauth_async_std::validate_token("access_token");
+/// ```
+pub async fn validate_token(
+    access_token: &str,
+) -> Result<ValidatedToken, Box<dyn std::error::Error>> {
+    let auth = format!("OAuth {}", access_token);
+
+    let client = surf::Client::new();
+    let resp: ValidatedToken = client
+        .get("https://id.twitch.tv/oauth2/validate")
+        .set_header("authorization", auth)
+        .recv_json()
+        .await?;
+
+    Ok(resp)
+}
+
+/// To remoke a token, you need to provide your access token and client_id
+///
+/// ```rust
+/// let token = twitch_oauth_async_std::remoke_token("token", "client_id");
+/// ```
+pub async fn remoke_token(
+    access_token: &str,
+    client_id: &str,
+) -> Result<surf::http_types::StatusCode, Box<dyn std::error::Error>> {
+    let mut params = HashMap::new();
+    params.insert("token", access_token);
+    params.insert("client_id", client_id);
+
+    let url = Url::parse_with_params("https://id.twitch.tv/oauth2/revoke", &params).unwrap();
+
+    let client = surf::Client::new();
+    let resp = client.post(&url).await?;
+    Ok(resp.status())
+}
